36d49362bda1b6dca8bfce59d6c4ed58b9d0797d
Anh Duong Tien <anhdt1911.work@gmail.com>
2023-04-26 10:26:31 +0700

    finish

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use grep_rust::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = grep_rust::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1)
     }
 }
